0d1420c97444c843afd0ce3be5dbfb4dee6e4244
Peter Caisse <pcaisse@gmail.com>
2017-08-05 15:15:07 -0400

    Use ternary to update matches counts

--- static/game.js
+++ static/game.js
@@ -15,13 +15,13 @@ document.body.children[0].addEventListener('click', function(e) {
   // Count matches
   for (i = 0; i < 7; i++) {
     // Horizontal matches
-    matchesHoriz = playerTurn == grid[rowIndex][i] && matchesHoriz + 1 || 0
+    matchesHoriz = playerTurn == grid[rowIndex][i] ? matchesHoriz + 1 : 0
     if (i < 6) {
       // Vertical matches
-      matchesVert = playerTurn == grid[i][colIndex] && matchesVert + 1 || 0
+      matchesVert = playerTurn == grid[i][colIndex] ? matchesVert + 1 : 0
       // Diagonal matches
-      matchesDiagonal1 = playerTurn == grid[i][colIndex + rowIndex - i] && matchesDiagonal1 + 1 || 0
-      matchesDiagonal2 = playerTurn == grid[i][colIndex - rowIndex + i] && matchesDiagonal2 + 1 || 0
+      matchesDiagonal1 = playerTurn == grid[i][colIndex + rowIndex - i] ? matchesDiagonal1 + 1 : 0
+      matchesDiagonal2 = playerTurn == grid[i][colIndex - rowIndex + i] ? matchesDiagonal2 + 1 : 0
     }
     // Check win
     Math.max(matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2) > 3 && gameOver++
