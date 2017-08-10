9123dbb1ce7ce3a5596f2372d56f970d4d37a58d
Peter Caisse <pcaisse@gmail.com>
2017-08-10 11:05:13 -0400

    Set playerTurn to 1 or 2 instead of 0 or 1

--- static/game.js
+++ static/game.js
@@ -1,13 +1,11 @@
-var grid = [[], [], [], [], [], []], i, colIndex, rowIndex, gridString, value
-var playerTurn = 0
-var gameOver = 0
+var grid = [[], [], [], [], [], []], i, colIndex, rowIndex, gridString, value, playerTurn = 1, gameOver = 0
 
 document.body.children[0].addEventListener('click', function(e) {
   colIndex = e.target.cellIndex
   if (!gameOver && +(colIndex + 1)) {
     // Drop into column
     rowIndex = 6
-    while (grid[--rowIndex] && grid[rowIndex][colIndex] + 1);
+    while (grid[--rowIndex] && grid[rowIndex][colIndex]);
 
     if (rowIndex >= 0) {
       // Keep track of latest move
@@ -16,13 +14,13 @@ document.body.children[0].addEventListener('click', function(e) {
       gridString = ''
       for (i = 0; i < 42; i++) {
         value = grid[~~(i / 7)][i % 7]
-        gridString += +(value + 1) ? value : " "
+        gridString += value || " "
       }
       /(\d)(\1{3}|(.{6}\1){3}|(.{7}\1){3}|(.{5}\1){3})/.exec(gridString) && ++gameOver && console.log('WINNER!')
-      // Switch turns
-      playerTurn = +!playerTurn
       // Update board
-      this.rows[rowIndex].cells[colIndex].style.background = playerTurn ? "#ff0" : "red"
+      this.rows[rowIndex].cells[colIndex].style.background = playerTurn > 1 ? "red" : "#ff0"
+      // Switch turns
+      playerTurn = playerTurn > 1 ? 1 : 2
     }
   }
 })
