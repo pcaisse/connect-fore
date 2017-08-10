2d2ce81409aabdc61d2d9dd308e5ffb32fc64ca1
Peter Caisse <pcaisse@gmail.com>
2017-08-09 22:21:04 -0400

    Get rid of try/catch (it was fun, but too long!)

--- static/game.js
+++ static/game.js
@@ -8,33 +8,33 @@ var matchesDiagonal2 = 0
 
 document.body.children[0].addEventListener('click', function(e) {
   colIndex = e.target.cellIndex
-  try {
-    (gameOver || !+(colIndex + 1)) && z
+  if (!gameOver && +(colIndex + 1)) {
     // Drop into column
     rowIndex = 6
-    while (grid[--rowIndex][colIndex] + 1);
-  } catch (e) {
-    return
-  }
-  // Keep track of latest move
-  grid[rowIndex][colIndex] = playerTurn
-  // Count matches
-  for (i = 7; i--;) {
-    // Horizontal matches
-    matchesHoriz = playerTurn == grid[rowIndex][i] ? matchesHoriz + 1 : 0
-    if (i < 6) {
-      // Vertical matches
-      matchesVert = playerTurn == grid[i][colIndex] ? matchesVert + 1 : 0
-      // Diagonal matches
-      matchesDiagonal1 = playerTurn == grid[i][colIndex + rowIndex - i] ? matchesDiagonal1 + 1 : 0
-      matchesDiagonal2 = playerTurn == grid[i][colIndex - rowIndex + i] ? matchesDiagonal2 + 1 : 0
+    while (grid[--rowIndex] && grid[rowIndex][colIndex] + 1);
+
+    if (rowIndex >= 0) {
+      // Keep track of latest move
+      grid[rowIndex][colIndex] = playerTurn
+      // Count matches
+      for (i = 7; i--;) {
+        // Horizontal matches
+        matchesHoriz = playerTurn == grid[rowIndex][i] ? matchesHoriz + 1 : 0
+        if (i < 6) {
+          // Vertical matches
+          matchesVert = playerTurn == grid[i][colIndex] ? matchesVert + 1 : 0
+          // Diagonal matches
+          matchesDiagonal1 = playerTurn == grid[i][colIndex + rowIndex - i] ? matchesDiagonal1 + 1 : 0
+          matchesDiagonal2 = playerTurn == grid[i][colIndex - rowIndex + i] ? matchesDiagonal2 + 1 : 0
+        }
+        // Check win
+        Math.max(matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2) > 3 && gameOver++
+      }
+      gameOver && console.log('WINNER!')
+      // Switch turns
+      playerTurn = +!playerTurn
+      // Update board
+      this.rows[rowIndex].cells[colIndex].style.background = playerTurn ? "#ff0" : "red"
     }
-    // Check win
-    Math.max(matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2) > 3 && gameOver++
   }
-  gameOver && console.log('WINNER!')
-  // Switch turns
-  playerTurn = +!playerTurn
-  // Update board
-  this.rows[rowIndex].cells[colIndex].style.background = playerTurn ? "#ff0" : "red"
 })
