a23c67d3f483fbd27f868179299c5722f25f7ab2
Peter Caisse <pcaisse@gmail.com>
2017-08-15 16:29:56 -0400

    Go back to playerTurn values of 0 and 1

--- static/game.js
+++ static/game.js
@@ -1,11 +1,11 @@
-grid = [[], [], [], [], [], []], playerTurn = 1, gameOver = 0
+grid = [[], [], [], [], [], []], playerTurn = 0, gameOver = 0
 
 document.body.children[0].onclick = function(e) {
   colIndex = e.target.cellIndex
   if (!gameOver && colIndex + 1) {
     // Drop into column
     rowIndex = 6
-    while (grid[--rowIndex] && grid[rowIndex][colIndex]);
+    while (grid[--rowIndex] && grid[rowIndex][colIndex] + 1);
 
     if (rowIndex >= 0) {
       // Keep track of latest move
@@ -13,13 +13,13 @@ document.body.children[0].onclick = function(e) {
       // Count matches
       gridString = ''
       for (i = 42; i--;) {
-        gridString = (grid[~~(i / 7)][i % 7] || " ") + gridString
+        gridString = (grid[~~(i / 7)][i % 7] + 1 || " ") + gridString
       }
       /(\d)(\1{3}|(.{6}\1){3}|(.{7}\1){3}|(.{5}\1){3})/.exec(gridString) && ++gameOver && console.log('WINNER!')
       // Update board
-      this.rows[rowIndex].cells[colIndex].style.background = playerTurn > 1 ? "red" : "#ff0"
+      this.rows[rowIndex].cells[colIndex].style.background = playerTurn ? "red" : "#ff0"
       // Switch turns
-      playerTurn = playerTurn > 1 ? 1 : 2
+      playerTurn = +!playerTurn
     }
   }
 }
