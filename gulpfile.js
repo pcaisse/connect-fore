a190fc57a87f980bea3d5226139d292a7299b383
Peter Caisse <pcaisse@gmail.com>
2017-08-16 00:08:49 -0400

    Update commands and build steps to beautify instead of minify

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,19 @@ var uglify = require('gulp-uglify')
 var file = 'static/game.js'
 var dist = 'static/dist'
 
-gulp.task('dev', function() {
+gulp.task('build-dev', function() {
   return gulp.src(file)
+    .pipe(uglify({
+      output: {
+        beautify: true,
+        indent_level: 2,
+        bracketize: true
+      }
+    }))
     .pipe(gulp.dest(dist))
 })
 
 gulp.task('build', function() {
   return gulp.src(file)
-    .pipe(uglify({
-      mangle: {
-        toplevel: true
-      }
-    }))
     .pipe(gulp.dest(dist))
 })
