02baf315eac2307667994c638484324dfeec4830
Peter Caisse <pcaisse@gmail.com>
2017-08-23 12:18:35 -0400

    Inline updateMatches function

--- static/game.js
+++ static/game.js
@@ -2,10 +2,6 @@ var playerTurn = 0
 var grid = [[], [], [], [], [], []]
 var gameOver = 0
 
-function updateMatches(value, matches) {
-  return value == playerTurn ? (matches || 0) + 1 : 0
-}
-
 document.body.children[0].onclick = function(e) {
   var colIndex = e.target.cellIndex
   if (gameOver || colIndex == void 0) {
@@ -25,18 +21,16 @@ document.body.children[0].onclick = function(e) {
   // Keep track of latest move
   grid[rowIndex][colIndex] = playerTurn
   // Count matches
-  var matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2
+  var matchesHoriz = matchesVert = matchesDiagonal1 = matchesDiagonal2 = 0
   for (i = 0; i < 7; i++) {
     // Horizontal matches
-    matchesHoriz = updateMatches(grid[rowIndex][i], matchesHoriz)
+    matchesHoriz = playerTurn == grid[rowIndex][i] && matchesHoriz + 1 || 0
     if (i < 6) {
       // Vertical matches
-      matchesVert = updateMatches(grid[i][colIndex], matchesVert)
+      matchesVert = playerTurn == grid[i][colIndex] && matchesVert + 1 || 0
       // Diagonal matches
-      matchesDiagonal1 = updateMatches(
-        grid[i][colIndex + rowIndex - i], matchesDiagonal1)
-      matchesDiagonal2 = updateMatches(
-        grid[i][colIndex - rowIndex + i], matchesDiagonal2)
+      matchesDiagonal1 = playerTurn == grid[i][colIndex + rowIndex - i] && matchesDiagonal1 + 1 || 0
+      matchesDiagonal2 = playerTurn == grid[i][colIndex - rowIndex + i] && matchesDiagonal2 + 1 || 0
     }
     // Check win
     Math.max(matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2) > 3 && gameOver++
