1fddb03c056005e47f30d86115d37bc1056bab3e
Peter Caisse <pcaisse@gmail.com>
2017-08-23 13:19:37 -0400

    Have a single exit point for the function

--- static/game.js
+++ static/game.js
@@ -4,36 +4,34 @@ var gameOver = 0
 
 document.body.children[0].onclick = function(e) {
   var colIndex = e.target.cellIndex
-  if (gameOver || colIndex == void 0) {
-    return
-  }
-  // Drop into column
-  rowIndex = 6
-  while (grid[--rowIndex] && grid[rowIndex][colIndex] + 1);
-  if (rowIndex == void 0) {
-    return
-  }
-  // Keep track of latest move
-  grid[rowIndex][colIndex] = playerTurn
-  // Count matches
-  var matchesHoriz = matchesVert = matchesDiagonal1 = matchesDiagonal2 = 0
-  for (i = 0; i < 7; i++) {
-    // Horizontal matches
-    matchesHoriz = playerTurn == grid[rowIndex][i] && matchesHoriz + 1 || 0
-    if (i < 6) {
-      // Vertical matches
-      matchesVert = playerTurn == grid[i][colIndex] && matchesVert + 1 || 0
-      // Diagonal matches
-      matchesDiagonal1 = playerTurn == grid[i][colIndex + rowIndex - i] && matchesDiagonal1 + 1 || 0
-      matchesDiagonal2 = playerTurn == grid[i][colIndex - rowIndex + i] && matchesDiagonal2 + 1 || 0
+  if (!gameOver && colIndex + 1) {
+    // Drop into column
+    rowIndex = 6
+    while (grid[--rowIndex] && grid[rowIndex][colIndex] + 1);
+    if (rowIndex + 1) {
+      // Keep track of latest move
+      grid[rowIndex][colIndex] = playerTurn
+      // Count matches
+      var matchesHoriz = matchesVert = matchesDiagonal1 = matchesDiagonal2 = 0
+      for (i = 0; i < 7; i++) {
+        // Horizontal matches
+        matchesHoriz = playerTurn == grid[rowIndex][i] && matchesHoriz + 1 || 0
+        if (i < 6) {
+          // Vertical matches
+          matchesVert = playerTurn == grid[i][colIndex] && matchesVert + 1 || 0
+          // Diagonal matches
+          matchesDiagonal1 = playerTurn == grid[i][colIndex + rowIndex - i] && matchesDiagonal1 + 1 || 0
+          matchesDiagonal2 = playerTurn == grid[i][colIndex - rowIndex + i] && matchesDiagonal2 + 1 || 0
+        }
+        // Check win
+        Math.max(matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2) > 3 && gameOver++
+      }
+      gameOver && console.log('WINNER!')
+      // Update board
+      this.rows[rowIndex].cells[colIndex].style.background =
+        playerTurn ? "red" : "#ff0"
+      // Switch turns
+      playerTurn = +!playerTurn
     }
-    // Check win
-    Math.max(matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2) > 3 && gameOver++
   }
-  gameOver && console.log('WINNER!')
-  // Update board
-  this.rows[rowIndex].cells[colIndex].style.background =
-    playerTurn ? "red" : "#ff0"
-  // Switch turns
-  playerTurn = +!playerTurn
 }
