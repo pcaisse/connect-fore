b06e2d546cc510d4833c3aca2a086293fa841a2f
Peter Caisse <pcaisse@gmail.com>
2017-08-23 11:33:49 -0400

    Basic implementation

--- static/game.js
+++ static/game.js
@@ -1,1 +1,62 @@
-t=0,g=Array(43).join(" "),o=0,document.body.children[0].onclick=function(e){if(c=e.target.cellIndex,!o&&c+1){for(i=35+c;i>=0&&" "!=g[i];i-=7);i>=0&&(g=g.slice(0,i)+t+g.slice(i+1),/(\d)(\1{3}|(.{6}\1){3}|(.{7}\1){3}|(.{5}\1){3})/.exec(g)&&++o&&console.log("WINNER!"),this.rows[~~(i/7)].cells[i%7].style.background=t?"red":"#ff0",t=+!t)}}
+var COLS = 7
+var ROWS = 6
+var MATCHES_TO_WIN = 4
+
+var playerTurn = 0
+var grid = [[], [], [], [], [], []]
+var gameOver = false
+
+function updateMatches(value, matches) {
+  return value === playerTurn ? (matches || 0) + 1 : 0
+}
+
+var table = document.querySelector('table')
+table.addEventListener('click', function(e) {
+  var colIndex = e.target.cellIndex
+  if (gameOver || typeof colIndex === "undefined") {
+    return
+  }
+  // Drop into column
+  var i, rowIndex
+  for (i = ROWS - 1; i >= 0; i--) {
+    if (typeof grid[i][colIndex] === "undefined") {
+      rowIndex = i
+      break
+    }
+  }
+  if (typeof rowIndex === "undefined") {
+    return
+  }
+  // Keep track of latest move
+  grid[rowIndex][colIndex] = playerTurn
+  // Count matches
+  var matchesHoriz, matchesVert, matchesDiagonal1, matchesDiagonal2
+  for (i = 0; i < COLS; i++) {
+    // Horizontal matches
+    matchesHoriz = updateMatches(grid[rowIndex][i], matchesHoriz)
+    if (i < ROWS) {
+      // Vertical matches
+      matchesVert = updateMatches(grid[i][colIndex], matchesVert)
+      // Diagonal matches
+      matchesDiagonal1 = updateMatches(
+        grid[i][colIndex + rowIndex - i], matchesDiagonal1)
+      matchesDiagonal2 = updateMatches(
+        grid[i][colIndex - rowIndex + i], matchesDiagonal2)
+    }
+    // Check win
+    if (matchesHoriz >= MATCHES_TO_WIN ||
+        matchesVert >= MATCHES_TO_WIN ||
+        matchesDiagonal1 >= MATCHES_TO_WIN ||
+        matchesDiagonal2 >= MATCHES_TO_WIN) {
+      gameOver = true
+    }
+  }
+  if (gameOver) {
+    console.log('WINNER!')
+  }
+  // Update board
+  table.rows[rowIndex].cells[colIndex].style.backgroundColor =
+    playerTurn ? "red" : "yellow"
+  // Switch turns
+  playerTurn = playerTurn ? 0 : 1
+})
